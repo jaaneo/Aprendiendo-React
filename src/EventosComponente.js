645862d8419fca67840ee33025f79372172bafdf
Alejandro <jaaneo@gmail.com>
2023-05-23 00:16:06 -0400

    doubleclick event

--- src/EventosComponente.js
+++ src/EventosComponente.js
@@ -6,11 +6,21 @@ export const EventosComponente = () => {
     alert(nombre+" has dado click al botón")
   }
 
+  const handleDobleClick = (e) => {
+    alert(" has dado doble click")
+  }
+
   return (
     <div>
       <h1>Eventos en React</h1>
-      {/* Evebti click*/}
-      <button onClick={ e => handleClick(e, "Alejandro") }>Dame click</button>
+      <p>
+        {/* Evento click*/}
+        <button onClick={ e => handleClick(e, "Alejandro") }>Dame click</button>
+      </p>
+      <p>
+        {/* Evento dobleClick*/}
+        <button onDoubleClick={ handleDobleClick }>Dame doble click</button>
+      </p>
     </div>
   )
 }
